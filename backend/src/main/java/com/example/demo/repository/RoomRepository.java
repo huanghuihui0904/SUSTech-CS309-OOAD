e5cad9e1e7eedd0fd1e427f6a594289593b7cc9c
HuangFF <625173@qq.com>
2022-11-03 11:21:34 +0800

    实现根据城市分店信息查询房间

--- backend/src/main/java/com/example/demo/repository/RoomRepository.java
+++ backend/src/main/java/com/example/demo/repository/RoomRepository.java
@@ -2,10 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Hotel;
 import com.example.demo.entity.Room;
+import com.example.demo.entity.cityHotelRoom;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room,Integer> {
@@ -14,4 +17,6 @@ public interface RoomRepository extends JpaRepository<Room,Integer> {
 
   //    @Query(value = "select * from orders",nativeQuery = true)
   public List<Room> findAll();
+
+
 }
