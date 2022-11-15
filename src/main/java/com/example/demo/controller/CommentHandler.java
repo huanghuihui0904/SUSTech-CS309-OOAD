20621b11ae33c79fa678ad4c83ba8d4a2c3919be
MichaelZhangJiahao <michaelzjh2002@gmail.com>
2022-11-15 17:27:21 +0800

    merge chatroom into backend

--- src/main/java/com/example/demo/controller/CommentHandler.java
+++ src/main/java/com/example/demo/controller/CommentHandler.java
@@ -19,23 +19,22 @@ public class CommentHandler {
     JdbcTemplate jdbcTemplate;
 
 
-
-    @GetMapping( "/{id}")
-    public Comment getbyid(@PathVariable("id") int id){
+    @GetMapping("/{id}")
+    public Comment getbyid(@PathVariable("id") int id) {
         return commentRepository.findAllByCommentid(id);
     }
 
     @GetMapping("/findAll")
-    public List findAll(){
-        List<Comment> comments= commentRepository.findAll();
+    public List findAll() {
+        List<Comment> comments = commentRepository.findAll();
         return comments;
     }
 
     @Delete("/deletebyid")
-    public String deletebyid(@RequestParam("id") int id){
+    public String deletebyid(@RequestParam("id") int id) {
         // 删除语句
         String sql = "delete from comment where commentid=?";
-        jdbcTemplate.update(sql,id);
+        jdbcTemplate.update(sql, id);
         // 查询
         return "delete by id Ok";
 
@@ -43,17 +42,16 @@ public class CommentHandler {
     }
 
 
-
     @PostMapping("/insert")
-    public String addComment(@RequestBody Comment comment){
+    public String addComment(@RequestBody Comment comment) {
 
         Integer maxId = jdbcTemplate.queryForObject("select MAX(commentid) from comment", Integer.class);
-        if (maxId==null)maxId=0;
-        comment.setCommentid(maxId+1);
+        if (maxId == null) maxId = 0;
+        comment.setCommentid(maxId + 1);
         Comment result = commentRepository.save(comment);
-        if(result!=null){
+        if (result != null) {
             return "insert ok";
-        }else {
+        } else {
             return "insert fail";
         }
     }
