7a05fb17cdc1a650422b8bae663468967e22a244
qwertyuiop41 <94441678+qwertyuiop41@users.noreply.github.com>
2022-11-19 14:57:20 +0800

    41

--- src/main/java/com/example/demo/controller/CustomerHandler.java
+++ src/main/java/com/example/demo/controller/CustomerHandler.java
@@ -1,34 +1,34 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Chat;
-import com.example.demo.entity.Comment;
-import com.example.demo.entity.Hotel;
-import com.example.demo.entity.Orders;
-import com.example.demo.repository.CommentRepository;
-import com.example.demo.repository.HotelRepository;
-import com.example.demo.repository.OrdersRepository;
-import org.apache.ibatis.annotations.Delete;
+import com.example.demo.entity.*;
+import com.example.demo.repository.*;
+import lombok.Data;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RestController
-@RequestMapping("/comment")
-public class CommentHandler {
+@RequestMapping(value = "/customer")
+public class CustomerHandler {
     @Autowired
-    CommentRepository commentRepository;
+    CustomerRepository customerRepository;
 
     @Autowired
     HotelRepository hotelRepository;
+
+    @Autowired
+    HotelWishListRepository hotelWishListRepository;
+
+    @Autowired
+    RoomTypeRepository roomTypeRepository;
+
+    @Autowired
+    RoomTypeWishListRepository roomTypeWishListRepository;
 
     @Autowired
     OrdersRepository ordersRepository;
@@ -36,58 +36,176 @@ public class CommentHandler {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    @RequestMapping(value = "/getbyid",method = RequestMethod.GET)
+    public Customer getbyid(@RequestParam("id") int id){
+        Customer customer=  customerRepository.findByCustomerid(id);
+        return customer;
+    }
 
 
-    @GetMapping( "/{id}")
-    public List<Comment> getbyid(@PathVariable("id") int id){
-        Hotel hotel=hotelRepository.findHotelByHotelid(id);
-        List<Orders> orders=ordersRepository.findOrdersByHotelid(id);
-        List<Comment>commentList=new ArrayList<>();
-        for (Orders o:
-             orders) {
-            Integer commentid=o.getCommentid();
-            if (commentid!=null){
-                Comment comment=commentRepository.findAllByCommentid(commentid);
-                if (comment!=null){
-                    commentList.add(comment);
-                }
 
-            }
+    @RequestMapping(value = "/getbyparameters",method = RequestMethod.GET)
+    public List<CustomerInfoA> getByParameters(@RequestParam(required = false,value = "id") Integer id, @RequestParam(required = false,value = "customerName")String customerName, @RequestParam(required = false,value = "telephone") String telephone){
+        if (id==null&&(customerName==null||customerName.equals(""))&&(telephone==null||telephone.equals(""))){
+            return null;
+        }
+        List<Customer> customers=customerRepository.findAll();
+        if (id!=null){
+            List<Customer> customerbyid=  customerRepository.findCustomersByCustomerid(id);
+            customers= (List<Customer>) CollectionUtils.intersection(customers,customerbyid);
+            customers=customerbyid;
 
         }
-        return commentList;
+        if (customerName!=null && !customerName.equals("")){
+            List<Customer> customerbyname=customerRepository.findCustomersByName(customerName);
+            customers= (List<Customer>) CollectionUtils.intersection(customerbyname,customers);
+        }
+        if (telephone!=null && !telephone.equals("")){
+            List<Customer> customerbytelephone=customerRepository.findCustomersByTelephone(telephone);
+            customers= (List<Customer>) CollectionUtils.intersection(customerbytelephone,customers);
+        }
+        List<CustomerInfoA> fin=new ArrayList<>();
+        for (Customer c:
+             customers) {
+            fin.add(new CustomerInfoA(c));
+        }
+        return fin;
+
     }
+
 
     @GetMapping("/findAll")
-    public List findAll(){
-        List<Comment> comments= commentRepository.findAll();
-        return comments;
+    public List<Customer> findAll(){
+        return customerRepository.findAll();
     }
 
-    @Delete("/deletebyid")
+    @PostMapping("/deletebyid")
     public String deletebyid(@RequestParam("id") int id){
         // 删除语句
-        String sql = "delete from comment where commentid=?";
+        String sql = "delete from customer where customerid=?";
         jdbcTemplate.update(sql,id);
         // 查询
         return "delete by id Ok";
+    }
 
+    @PostMapping("/money")
+    public boolean modifyMoney(@RequestParam("id") int id,@RequestParam("money")int money){
+        // 删除语句
+        Customer customer=customerRepository.findByCustomerid(id);
+        Integer currentMoney=customer.getMoney();
+        if (currentMoney+money>=0){
+            String sql = "update customer set money=?  where customerid=?";
+            jdbcTemplate.update(sql,(money+currentMoney),id);
+            return true;
 
+        }
+
+        // 查询
+        return false;
+    }
+
+    @PostMapping("/credits")
+    public boolean addCredits(@RequestParam("id") int id,@RequestParam("credits")int credits){
+        // 删除语句
+        Customer customer=customerRepository.findByCustomerid(id);
+        Integer currentCredits=customer.getCredits();
+        if (currentCredits+credits>=0){
+            String sql = "update customer set credits=?  where customerid=?";
+            jdbcTemplate.update(sql,(credits+currentCredits),id);
+            return true;
+        }
+
+        // 查询
+        return false;
     }
 
 
 
-    @PostMapping("/insert")
-    public String addComment(@RequestBody Comment comment){
 
-        Integer maxId = jdbcTemplate.queryForObject("select MAX(commentid) from comment", Integer.class);
+
+
+    @PostMapping("/createcustomer")
+    public boolean createCustomer(@RequestBody CustomerInfo customerInfo){
+
+        Integer maxId = jdbcTemplate.queryForObject("select MAX(customerid) from customer", Integer.class);
         if (maxId==null)maxId=0;
-        comment.setCommentid(maxId+1);
-        Comment result = commentRepository.save(comment);
-        if(result!=null){
-            return "insert ok";
+        Customer customer=new Customer(0,0,customerInfo.getName(),customerInfo.getLoginpassword(),customerInfo.getTelephone(),0,0);
+        customer.setCustomerid(maxId+1);
+        List<Customer> customer1=customerRepository.findCustomersByName(customerInfo.getName());
+        List<Customer> customer2=customerRepository.findCustomersByTelephone(customerInfo.getTelephone());
+        Collection<Customer>customers=CollectionUtils.intersection(customer1,customer2);
+        if ( customers.size()==0){
+            Customer result = customerRepository.save(customer);
+            if(result!=null){
+                return true;
+            }else {
+
+
+                return false;
+            }
         }else {
-            return "insert fail";
+            return false;
+        }
+
+    }
+
+    @Data
+    static
+    class CustomerInfo{
+        private String name;
+        private String loginpassword;
+        private String telephone;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLoginpassword() {
+            return loginpassword;
+        }
+
+        public void setLoginpassword(String loginpassword) {
+            this.loginpassword = loginpassword;
+        }
+
+        public String getTelephone() {
+            return telephone;
+        }
+
+        public void setTelephone(String telephone) {
+            this.telephone = telephone;
+        }
+    }
+
+    @Data
+     class CustomerInfoA{
+        Integer customerid;
+        String customerName;
+        String telephone;
+        String favoriteRoomType;
+        String favoriteHotel;
+        List<Orders> orders;
+
+
+        public CustomerInfoA(Customer customer) {
+            customerid=customer.getCustomerid();
+            customerName=customer.getName();
+            telephone=customer.getTelephone();
+            RoomTypeWishList roomTypeWishList=roomTypeWishListRepository.findRoomTypeWishListByRoomtypewishlistid(customer.getRoomtypewishlistid());
+            if ( roomTypeWishList!=null){
+                favoriteRoomType=roomTypeRepository.findRoomTypeByRoomtypeid(roomTypeWishList.getRoomtypeid()).getRoomname();
+            }
+            HotelWishList hotelWishList=hotelWishListRepository.findHotelWishListByHotelwishlistid(customer.getHotelwishlistid());
+            if (hotelWishList!=null){
+                favoriteHotel=hotelRepository.findHotelByHotelid(hotelWishList.getHotelid()).getHotelname();
+            }
+
+            orders=ordersRepository.findOrdersByCustomerid(customer.getCustomerid());
+
         }
     }
 
