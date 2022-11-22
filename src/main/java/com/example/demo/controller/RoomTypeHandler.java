45220f7aa2509c9473cb5b7b25ed68e4a6e8aab4
HuangFF <625173@qq.com>
2022-11-22 22:04:23 +0800

    添加json工具类

--- src/main/java/com/example/demo/controller/RoomTypeHandler.java
+++ src/main/java/com/example/demo/controller/RoomTypeHandler.java
@@ -1,16 +1,13 @@
 package com.example.demo.controller;
 
-
 import com.example.demo.entity.Hotel;
 import com.example.demo.entity.RoomType;
 import com.example.demo.repository.HotelRepository;
 import com.example.demo.repository.RoomTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
-
+import com.example.demo.JsonUtil;
 import javax.transaction.Transactional;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
@@ -44,8 +41,8 @@ public class RoomTypeHandler {
   public List<RoomType> byHotel(@RequestParam("hotelName") String hotelName) throws UnsupportedEncodingException {
     System.out.println(hotelName);
 
-    List<Hotel> hotels=hotelRepository.findHotelsByHotelname(hotelName);
-    int hotelId=hotels.get(0).getHotelid();
+    List<Hotel> hotels = hotelRepository.findHotelsByHotelname(hotelName);
+    int hotelId = hotels.get(0).getHotelid();
     System.out.println(hotelId);
     List<RoomType> roomTypes = roomTypeRepository.findRoomTypeByHotelid(hotelId);
 
@@ -53,99 +50,123 @@ public class RoomTypeHandler {
   }
 
 
-
-
   @PostMapping("/addRoom")
-  public String addRoom(@RequestBody String str) throws JSONException {
-    JSONObject js=new JSONObject(str);
-    String hotelName=js.getString("hotelName");
-    String roomName=js.getString("roomName");
-    Integer price= Integer.valueOf(js.getString("price"));
-    Integer remain= Integer.valueOf(js.getString("remain"));
-    String introduction=js.getString("introduction");
-    Integer number= Integer.valueOf(js.getString("number"));
-List<Hotel> hotels=hotelRepository.findHotelsByHotelname(hotelName);
-int hotelId=hotels.get(0).getHotelid();
-RoomType newRoom=new RoomType(hotelId,roomName,remain,price,introduction,number);
-RoomType re=roomTypeRepository.save(newRoom);
-if(re!=null){
-  return "add ok";
-}else {
-  return "add fail";
-}
-
+  public String addRoom(@RequestBody String str) {
+    List<String[]> js=JsonUtil.decodeJSON(str);
+//    for (int i = 0; i < js.size(); i++) {
+//      System.out.println(js.get(i)[0]);
+//      System.out.println(js.get(i)[1]);
+//    }
+    String hotelName ="";
+    String roomName="" ;
+    Integer price=0  ;
+    Integer remain =0 ;
+    String introduction="" ;
+    Integer number=0;
+    //匹配
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("hotelName")){
+        hotelName=js.get(i)[1];
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("roomName")){
+        roomName=js.get(i)[1];
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("price")){
+        price= Integer.valueOf(js.get(i)[1]);
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("remain")){
+        remain= Integer.valueOf(js.get(i)[1]);
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("introduction")){
+        introduction=js.get(i)[1];
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("number")){
+        number= Integer.valueOf(js.get(i)[1]);
+      }
+    }
+    //
+    List<Hotel> hotels = hotelRepository.findHotelsByHotelname(hotelName);
+    int hotelId = hotels.get(0).getHotelid();
+    RoomType newRoom = new RoomType(hotelId, roomName, remain, price, introduction, number);
+    RoomType re = roomTypeRepository.save(newRoom);
+    if (re != null) {
+      return "add ok";
+    } else {
+      return "add fail";
+    }
   }
 
 
   @PostMapping("/updateRoomType")
-  public String updateRoomType(@RequestBody String str) throws JSONException {
-    JSONObject js=new JSONObject(str);
-    int roomTypeId=  js.getInt("roomTypeId");
-    RoomType tempt=roomTypeRepository.findRoomTypeByRoomtypeid(roomTypeId);
+  public String updateRoomType(@RequestBody String str) {
+    List<String[]> js=JsonUtil.decodeJSON(str);
+
+    int roomTypeId = 0;
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("roomTypeId")){
+        roomTypeId= Integer.parseInt(js.get(i)[1]);
+      }
+    }
+    RoomType tempt = roomTypeRepository.findRoomTypeByRoomtypeid(roomTypeId);
 
     //
-    String roomTypeName=js.getString("roomTypeName");
-    int price= js.getInt("price");
-    String introduction=js.getString("introduction");
-    int number=js.getInt("number");
+    String roomTypeName = "";
+    int price =0;
+    String introduction = "";
+    int number = 0;
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("roomTypeName")){
+        roomTypeName= js.get(i)[1];
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("price")){
+        price= Integer.parseInt(js.get(i)[1]);
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("introduction")){
+        introduction= js.get(i)[1];
+      }
+    }
+    for (int i = 0; i <js.size() ; i++) {
+      if(js.get(i)[0].equals("number")){
+        number= Integer.parseInt(js.get(i)[1]);
+      }
+    }
 //
-    int hotelid=tempt.getHotelid();
-    int remain=tempt.getRemain();
+    int hotelid = tempt.getHotelid();
+    int remain = tempt.getRemain();
 
 
-    RoomType newRoomType=new RoomType(roomTypeId,hotelid,roomTypeName,remain,price,introduction,number);
-RoomType re=roomTypeRepository.save(newRoomType);
-if(re!=null){
-  return "update ok";
-}else {
-  return "update fail";
-}
+    RoomType newRoomType = new RoomType(roomTypeId, hotelid, roomTypeName, remain, price, introduction, number);
+    RoomType re = roomTypeRepository.save(newRoomType);
+    if (re != null) {
+      return "update ok";
+    } else {
+      return "update fail";
+    }
   }
 
-@GetMapping("/deleteRoom")
-@Transactional
-  public String deleteRoom(@RequestParam("roomID")int roomTypeId ){
-  System.out.println(roomTypeId);
-roomTypeRepository.deleteRoomTypeByRoomtypeid(roomTypeId);
-return "delete ok";
-}
+  @GetMapping("/deleteRoom")
+  @Transactional
+  public String deleteRoom(@RequestParam("roomID") int roomTypeId) {
+    System.out.println(roomTypeId);
+    roomTypeRepository.deleteRoomTypeByRoomtypeid(roomTypeId);
+    return "delete ok";
+  }
 
-//
-//
-//
-//  @PostMapping("insert")
-//  public String insert(@RequestBody RoomType roomType) {
-//
-//    Integer maxId = jdbcTemplate.queryForObject("select MAX(roomtypeid) from roomtype", Integer.class);
-//    roomType.setRoomtypeid(maxId+1);
-//    RoomType result = roomTypeRepository.save(roomType);
-//    if(result!=null){
-//      return "insert ok";
-//    }else {
-//      return "insert fail";
-//    }
-//
-//
-//  }
 
-//  @Data
-//  class AddRoomInfo{
-//    String hotelName;
-//    String roomName;
-//    Integer price;
-//    Integer remain;
-//    String introduction;
-//
-//    Integer number;
-//
-//    public AddRoomInfo(String hotelName, String roomName, Integer price, Integer remain, String introduction,Integer number) {
-//      this.hotelName = hotelName;
-//      this.roomName = roomName;
-//      this.price = price;
-//      this.remain = remain;
-//      this.introduction = introduction;
-//      this.number=number;
-//    }}
 }
 
 
