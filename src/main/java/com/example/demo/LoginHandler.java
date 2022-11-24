f9e8bfcd22b92bf317da39605b4fff0928ac81c0
HuangFF <625173@qq.com>
2022-11-24 13:07:46 +0800

    更新login接口

--- src/main/java/com/example/demo/LoginHandler.java
+++ src/main/java/com/example/demo/LoginHandler.java
@@ -4,12 +4,16 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.demo.entity.Customer;
 import com.example.demo.entity.Login;
 import com.example.demo.repository.CustomerRepository;
 import com.example.demo.repository.LoginRepository;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -27,20 +31,31 @@ public class LoginHandler {
   JdbcTemplate jdbcTemplate;
 
 
+@Data
+class loginInfo{
+  String token;
+  int id;
 
+  public loginInfo(String token, int id) {
+    this.token = token;
+    this.id = id;
+  }
+}
   @PostMapping
-  public String loginValidate(@RequestBody Login login) {
+  public loginInfo loginValidate(@RequestBody Login login) {
     String password = loginRepository.getPassword(login.getName());
     //去数据库找用户id
-//List<Customer>
+List<Customer> c=customerRepository.findCustomersByName(login.getName());
+
     //
     if (login.getLoginpassword() != null && password != null && login.getLoginpassword().equals(password)) {
       String token = jwtToken(login.getName());
       //将token放入redis缓存
       redisUtil.set(login.getName(), token, 100);
-      return token;
+      loginInfo re=new loginInfo(token,c.get(0).getCustomerid());
+      return re;
     } else {
-      return "login error";
+      return null;
     }
 
   }
